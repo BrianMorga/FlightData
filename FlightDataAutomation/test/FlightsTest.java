d1babf863f8356e2ba9c8b6144846c81ab8bc8cf
BrianMorga <bmorga@ggc.edu>
2023-12-06 17:52:01 -0500

    updated automation and sql query

--- FlightDataAutomation/test/FlightsTest.java
+++ FlightDataAutomation/test/FlightsTest.java
@@ -7,16 +7,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.sql.*;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
 import org.openqa.selenium.JavascriptExecutor;
 
 import static java.lang.Thread.sleep;
@@ -31,9 +28,9 @@ public class FlightsTest {
         System.setProperty("webdriver.gecko.driver", "geckodriver");
         driver = new FirefoxDriver();
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:flights.sqlite");
+            connection = DriverManager.getConnection("jdbc:sqlite:flights.db");
             Statement statement = connection.createStatement();
-            statement.executeUpdate("CREATE TABLE IF NOT EXISTS flights (id INTEGER PRIMARY KEY AUTOINCREMENT, airline_name TEXT, price TEXT)");
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS flights (id INTEGER PRIMARY KEY AUTOINCREMENT, destination TEXT, airline_name TEXT, price INTEGER, dates TEXT)");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -59,22 +56,26 @@ public class FlightsTest {
         searchButton.click();
 
         // Wait for the page to load - adjust the expected conditions based on the actual behavior of the page
-        wait.until(ExpectedConditions.urlContains("search"));
+        wait.until((ExpectedCondition<Boolean>) driver -> {
+            String currentUrl = driver.getCurrentUrl();
+            return currentUrl.contains("search") || currentUrl.contains("flights");
+        });
 
         for (int i = 0; i < 100; i++) {
             WebElement departureNextButton = driver.findElement(By.cssSelector(".uNiB1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(4) > button:nth-child(2)"));
             WebElement returnNextButton = driver.findElement(By.cssSelector(".uNiB1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > button:nth-child(2)"));
             departureNextButton.click();
             returnNextButton.click();
+            String departureDate = driver.findElement(By.cssSelector(".uNiB1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(2)")).getAttribute("value");
+            String returnDate = driver.findElement(By.cssSelector(".uNiB1 > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > input:nth-child(1)")).getAttribute("value");
+            String dateSlot = departureDate + " - " + returnDate;
             try {
                 sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            storeFlightInformation();
+            storeFlightInformation(destination, dateSlot);
         }
-
-
     }
 
     @Test
@@ -85,20 +86,12 @@ public class FlightsTest {
             String startDate = "May 1, 2024";
             String endDate = "May 7, 2024";
 
-
-
             performInitialSearch(startDate,endDate,destination,wait);
-
-
 
             // Perform search with the given departure and return date
             performFlightSearch(startDate, endDate, wait);
 
             sleep(1000);
-
-            // Get and store flight information
-
-
 
     }
 
@@ -124,20 +117,90 @@ public class FlightsTest {
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector(".uNiB1"))); // Assuming this element is present on the loaded page
     }
 
-    private void storeFlightInformation() {
-        String airlineName =driver.findElement(By.xpath("//*[@id=\"yDmH0d\"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[2]/div[3]/ul/li[1]/div/div[2]/div/div[2]/div[2]/div[2]")).getText();
+    private void storeFlightInformation(String destination, String dates) {
+        String airlineName =driver.findElement(By.cssSelector("ul.Rk10dc:nth-child(3) > li:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2)")).getText();
         String price = driver.findElement(By.cssSelector("ul.Rk10dc:nth-child(3) > li:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(6) > div:nth-child(1) > div:nth-child(2) > span:nth-child(1)")).getText();
+        int priceAsInteger = Integer.parseInt(price.replaceAll("[^0-9]", ""));
 
+        System.out.println("Destination: " + destination);
+        System.out.println("Dates: " + dates);
         System.out.println("Airline: " + airlineName);
         System.out.println("Price: " + price);
 
         // Store the information in the database
-        storeFlightInformation(airlineName, price);
+        storeFlightInformation(destination,airlineName, priceAsInteger,dates);
     }
 
-    private void storeFlightInformation(String airlineName, String price) {
-        try (Statement statement = connection.createStatement()) {
-            statement.executeUpdate("INSERT INTO flights (airline_name, price) VALUES ('" + airlineName + "', '" + price + "')");
+    private void storeFlightInformation(String destination, String airlineName, int price, String dates) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "INSERT INTO flights (destination, airline_name, price,dates) VALUES (?, ?, ?,?)")) {
+
+            preparedStatement.setString(1, destination);
+            preparedStatement.setString(2, airlineName);
+            preparedStatement.setInt(3, price);
+            preparedStatement.setString(4, dates);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void testCheapestTickets() {
+        getCheapestTickets();
+    }
+
+    @Test
+    public void testMostExpensiveTickets() {
+        getMostExpensiveTickets();
+    }
+
+    private void getCheapestTickets() {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT destination, airline_name, price, dates FROM flights " +
+                        "WHERE (destination, price) IN (SELECT destination, MIN(price) FROM flights GROUP BY destination)")) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String destination = resultSet.getString("destination");
+                String airlineName = resultSet.getString("airline_name");
+                String price = resultSet.getString("price");
+                String dates = resultSet.getString("dates");
+
+                System.out.println("Destination: " + destination +
+                        ", Airline: " + airlineName +
+                        ", Cheapest Price: $" + price +
+                        ", Dates: " + dates +
+                        "\n--------------------------");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void getMostExpensiveTickets() {
+        try (PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT destination, airline_name, price, dates FROM flights " +
+                        "WHERE (destination, price) IN (SELECT destination, MAX(price) FROM flights GROUP BY destination)")) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                String destination = resultSet.getString("destination");
+                String airlineName = resultSet.getString("airline_name");
+                String price = resultSet.getString("price");
+                String dates = resultSet.getString("dates");
+
+                System.out.println("Destination: " + destination +
+                        ", Airline: " + airlineName +
+                        ", Most Expensive Price: $" + price +
+                        ", Dates: " + dates +
+                        "\n--------------------------");
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
